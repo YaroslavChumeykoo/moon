b96ceb0006971a59c273457980b96d0e4eaf25d2
ярослав чумейко <00yasik00@gmail.com>
2024-03-07 01:24:40 +0300

    +menu+about

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
-const but_menu = document.querySelector('.but_menu')
 const main = document.querySelector('.main')
-const menu = document.querySelector('.menu')
-const moon = document.querySelector('.moon')
 const logo = document.querySelector('.logo')
+const but_about = document.querySelector('.but_about')
+const about_Us = document.querySelector('.about_Us')
+const body = document.querySelector('body')
+const but_menu = document.querySelector('.but_menu')
+const menu = document.querySelector('.menu')
 
 
-but_menu.addEventListener('click', clickMenu)
-moon.addEventListener('click', clickMenu)
+but_menu.addEventListener('click', goMenu)
+but_about.addEventListener('click', goAbout_Us)
 logo.addEventListener('click', goMain)
 
-function clickMenu(){
-    main.classList.toggle('none')
-    menu.classList.toggle('none')
-}
 function goMain(){
+    body.style.overflow = 'hidden'
     main.classList.remove('none')
+    about_Us.classList.add('none')
     menu.classList.add('none')
+}
+function goAbout_Us(){
+    body.style.overflow = "auto"
+    main.classList.add('none')
+    about_Us.classList.remove('none')
+    menu.classList.add('none')
+}
+function goMenu(){
+    body.style.overflow = "auto"
+    main.classList.add('none')
+    menu.classList.remove('none')
+    about_Us.classList.add('none')
 }
